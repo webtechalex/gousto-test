741fa38b720054df8bef47ca4927f6d4da8c52c7
Alex Brown <alexanderbrown.info@gmail.com>
2017-12-01 22:35:58 +0000

    test initial state of categories reducer

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,17 @@
 import {expect} from 'chai';
 
+import {categoriesReducer} from '../src/reducers';
+
 describe('environment', function() {
   it('should run a test', function() {
     expect(true).to.equal(true);
+  });
+});
+
+describe('categoriesReducer', function() {
+  it('should return initial state when called with no arguments', function() {
+    const categories = categoriesReducer();
+    expect(Array.isArray(categories));
+    expect(categories.length).to.equal(0);
   });
 });
