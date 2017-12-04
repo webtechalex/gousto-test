253372d7f78817100755e649c55d31c7221172c3
Alex Brown <alexanderbrown.info@gmail.com>
2017-12-04 10:48:29 +0000

    refactored product search input field handler and removed console log

--- src/components/ProductSearchInput.js
+++ src/components/ProductSearchInput.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function ProductSearchInput(props) {
-  return (<input value={props.productSearchInput} onChange={(event) => props.updateProductSearchInputValue(event.target.value)} />);
+  const handleUpdateProductSearchInput = (event) => props.updateProductSearchInputValue(event.target.value);
+
+  return (<input value={props.productSearchInput} onChange={handleUpdateProductSearchInput} />);
 }
 
 export default ProductSearchInput;
